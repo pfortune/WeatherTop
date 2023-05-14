7345f1d968a6a56dc23a2e3d62ec1a7afd487171
pfortune <pfortune@gmail.com>
2023-05-14 18:49:40 +0100

    Fixed permission issues on users accessing stations they don't own

--- app/controllers/StationController.java
+++ app/controllers/StationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import models.Reading;
 import models.Station;
 
+import models.User;
 import play.mvc.Controller;
 
 public class StationController extends Controller {
@@ -17,18 +18,32 @@ public class StationController extends Controller {
         }
 
         Station station = Station.findById(id);
-        if (station != null) {
+        if (station != null && station.user != null) {
+            String userID = session.get("logged_in_userid");
+            Long user = Long.parseLong(userID);
+            if(!station.user.id.equals(user)) {
+                flash("error", "You do not have permission to view this station");
+                redirect("/dashboard");
+            }
             render("station.html", station);
         } else {
-            flash("error", "Station not found");
+            flash("error", "You do not have permission to view this station");
             redirect("/dashboard");
         }
     }
 
 
+
     public static void addReading(Long id, int code, double temperature, double windSpeed, int pressure, int windDirection) {
         Station station = Station.findById(id);
         if (station != null) {
+            String userID = session.get("logged_in_userid");
+            Long user = Long.parseLong(userID);
+            if(!station.user.id.equals(user)) {
+                flash("error", "You do not have permission to access this station");
+                redirect("/dashboard");
+            }
+
             if(code == 0) {
                 flash("error", "Reading fields cannot be empty");
             } else {
