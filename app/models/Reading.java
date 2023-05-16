398f38f59e0c31d79c97aa1656b0c4db01f11176
Peter Fortune <pfortune@gmail.com>
2023-05-16 22:55:37 +0100

    Fixed issue with dates not loading correctly from seed yaml

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -5,8 +5,7 @@ import java.util.Date;
 import play.db.jpa.Model;
 
 @Entity
-public class Reading extends Model
-{
+public class Reading extends Model {
     public int code;
     public double temperature;
     public double windSpeed;
@@ -14,13 +13,16 @@ public class Reading extends Model
     public int windDirection;
     public Date date;
 
-    public Reading(int code, double temperature, double windSpeed, int pressure, int windDirection)
-    {
-        this.windSpeed = windSpeed;
+    public Reading(int code, double temperature, double windSpeed, int pressure, int windDirection, Date date) {
         this.code = code;
         this.temperature = temperature;
+        this.windSpeed = windSpeed;
         this.pressure = pressure;
         this.windDirection = windDirection;
-        this.date = new Date();
+        this.date = date;
+    }
+
+    public Reading(int code, double temperature, double windSpeed, int pressure, int windDirection) {
+        this(code, temperature, windSpeed, pressure, windDirection, new Date());
     }
 }
