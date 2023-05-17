b83fd811d30ee7832e1061f5ff9f8f3afff8cbd5
Peter Fortune <pfortune@gmail.com>
2023-05-17 12:17:34 +0100

    Added trending methods & added javadoc comments to Station model

--- app/models/Station.java
+++ app/models/Station.java
@@ -18,51 +18,172 @@ public class Station extends Model {
     @Required
     public double longitude;
 
+    /**
+     * A list of readings for this station.
+     * CascadeType.ALL means changes on Station will cascade to readings.
+     */
     @OneToMany(cascade = CascadeType.ALL)
     public List<Reading> readings = new ArrayList<Reading>();
 
+    /**
+     * Constructor for the Station class.
+     *
+     * @param name      The name of the station.
+     * @param latitude  The latitude of the station.
+     * @param longitude The longitude of the station.
+     */
     public Station(String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public Station() {
-
-    }
-
+    /**
+     * Gets the latest reading for this station.
+     *
+     * @return The latest Reading object.
+     */
     public Reading getLatestReading() {
         return readings.get(readings.size() - 1);
     }
 
+    /**
+     * Gets the latest weather description for this station.
+     *
+     * @return A string representing the latest weather condition.
+     */
     public String getLatestWeatherDescription() {
         return Conversion.weatherCodeToCondition(getLatestReading().code);
     }
 
+    /**
+     * Gets the latest temperature in Fahrenheit for this station.
+     *
+     * @return The latest temperature in Fahrenheit.
+     */
     public double getLatestTemperatureFahrenheit() {
         return Conversion.celsiusToFahrenheit(getLatestReading().temperature);
     }
 
+    /**
+     * Gets the latest temperature in Celsius for this station.
+     *
+     * @return The latest temperature in Celsius.
+     */
     public double getLatestTemperatureCelcius() {
         return getLatestReading().temperature;
     }
 
+    /**
+     * Gets the latest wind speed in Beaufort scale for this station.
+     *
+     * @return The latest wind speed in Beaufort scale.
+     */
     public int getLatestWindBeaufort() {
         return Conversion.kmhToBeaufort(getLatestReading().windSpeed);
     }
 
+    /**
+     * Gets the latest pressure reading for this station.
+     *
+     * @return The latest pressure reading.
+     */
     public int getLatestPressure() {
         return getLatestReading().pressure;
     }
 
+    /**
+     * Gets the latest wind direction in compass notation for this station.
+     *
+     * @return The latest wind direction in compass notation.
+     */
     public String getLatestWindDirectionCompass() {
         return Conversion.windDirectionToCompass(getLatestReading().windDirection);
     }
 
+    /**
+     * Gets the latest wind chill factor for this station.
+     *
+     * @return The latest wind chill factor.
+     */
     public double getLatestWindChill() {
         return Conversion.calculateWindChill(getLatestReading().temperature, getLatestReading().windSpeed);
     }
 
+    /**
+     * Gets the pressure trend for this station.
+     *
+     * @return A string representing the pressure trend.
+     */
+    public String getPressureTrend() {
+        if (readings.size() < 3) {
+            return "Insufficient data";
+        } else {
+            int lastReading = readings.get(readings.size() - 1).pressure;
+            int secondLastReading = readings.get(readings.size() - 2).pressure;
+            int thirdLastReading = readings.get(readings.size() - 3).pressure;
+
+            if ((lastReading - secondLastReading) > 0 && (secondLastReading - thirdLastReading) > 0) {
+                return "Increasing";
+            } else if ((lastReading - secondLastReading) < 0 && (secondLastReading - thirdLastReading) < 0) {
+                return "Decreasing";
+            } else {
+                return "Steady";
+            }
+        }
+    }
+
+    /**
+     * Gets the temperature trend for this station.
+     *
+     * @return A string representing the temperature trend.
+     */
+    public String getTemperatureTrend() {
+        if (readings.size() < 3) {
+            return "Insufficient data";
+        } else {
+            double lastReading = readings.get(readings.size() - 1).temperature;
+            double secondLastReading = readings.get(readings.size() - 2).temperature;
+            double thirdLastReading = readings.get(readings.size() - 3).temperature;
+
+            if ((lastReading - secondLastReading) > 0 && (secondLastReading - thirdLastReading) > 0) {
+                return "Increasing";
+            } else if ((lastReading - secondLastReading) < 0 && (secondLastReading - thirdLastReading) < 0) {
+                return "Decreasing";
+            } else {
+                return "Steady";
+            }
+        }
+    }
+
+    /**
+     * Gets the wind speed trend for this station.
+     *
+     * @return A string representing the wind speed trend.
+     */
+    public String getWindTrend() {
+        if (readings.size() < 3) {
+            return "Insufficient data";
+        } else {
+            double lastReading = readings.get(readings.size() - 1).windSpeed;
+            double secondLastReading = readings.get(readings.size() - 2).windSpeed;
+            double thirdLastReading = readings.get(readings.size() - 3).windSpeed;
+
+            if ((lastReading - secondLastReading) > 0 && (secondLastReading - thirdLastReading) > 0) {
+                return "Increasing";
+            } else if ((lastReading - secondLastReading) < 0 && (secondLastReading - thirdLastReading) < 0) {
+                return "Decreasing";
+            } else {
+                return "Steady";
+            }
+        }
+    }
+
+    /**
+     * Gets the maximum temperature recorded at this station.
+     *
+     * @return The maximum temperature.
+     */
     public double getMaxTemperature() {
         if (readings.size() > 0) {
             double maxTemp = readings.get(0).temperature;
@@ -77,6 +198,11 @@ public class Station extends Model {
         }
     }
 
+    /**
+     * Gets the minimum temperature recorded at this station.
+     *
+     * @return The minimum temperature.
+     */
     public double getMinTemperature() {
         if (readings.size() > 0) {
             double minTemp = readings.get(0).temperature;
@@ -91,6 +217,11 @@ public class Station extends Model {
         }
     }
 
+    /**
+     * Gets the maximum wind speed recorded at this station.
+     *
+     * @return The maximum wind speed.
+     */
     public double getMaxWindSpeed() {
         if (readings.size() > 0) {
             double maxWind = readings.get(0).windSpeed;
@@ -105,6 +236,11 @@ public class Station extends Model {
         }
     }
 
+    /**
+     * Gets the minimum wind speed recorded at this station.
+     *
+     * @return The minimum wind speed.
+     */
     public double getMinWindSpeed() {
         if (readings.size() > 0) {
             double minWind = readings.get(0).windSpeed;
@@ -119,6 +255,11 @@ public class Station extends Model {
         }
     }
 
+    /**
+     * Gets the maximum pressure recorded at this station.
+     *
+     * @return The maximum pressure.
+     */
     public double getMaxPressure() {
         if (readings.size() > 0) {
             double maxPressure = readings.get(0).pressure;
@@ -133,6 +274,11 @@ public class Station extends Model {
         }
     }
 
+    /**
+     * Gets the minimum pressure recorded at this station.
+     *
+     * @return The minimum pressure.
+     */
     public double getMinPressure() {
         if (readings.size() > 0) {
             double minPressure = readings.get(0).pressure;
@@ -146,6 +292,4 @@ public class Station extends Model {
             return 0;
         }
     }
-
-
 }
