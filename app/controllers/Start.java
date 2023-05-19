a598573317a8837f74a43be8260ad84af6034d1f
Peter Fortune <pfortune@gmail.com>
2023-05-19 10:58:28 +0100

    Change Position of Add Reading Form, and Add Redirect to Start

--- app/controllers/Start.java
+++ app/controllers/Start.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
 
 public class Start extends Controller {
   public static void index() {
+    User user = AuthController.getLoggedInUser();
+    if (user != null) {
+      redirect("/dashboard");
+    }
+
     render("start.html");
   }
 }
